ab270effd20861882fe89fa07a01be766f6528e2
CV Rohit <51963891+rohitchatla@users.noreply.github.com>
2019-11-18 13:09:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<div>
+	<form action="send_email.php" method="post">
+		<input type="text" name="name" name="email">
+		<input type="text" name="email" name="email">
+		<input type="text" name="subject" name="email">
+		<input type="text" name="message" name="email" placeholder="enter message">
+		<input type="submit" name="submit" value="send">
+	</form>	
+	</div>
+</body>
+</html>
